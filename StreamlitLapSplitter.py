13c86963a2ca12ce801beb6d459581490bdfe9b2
aerc4d <112525545+aerc4d@users.noreply.github.com>
2023-05-07 21:15:36 -0500

    Add files via upload

--- StreamlitLapSplitter.py
+++ StreamlitLapSplitter.py
@@ -2,31 +2,24 @@
 
 import streamlit as st
 
-st.markdown("# This will split laps")
-st.markdown("""This is to split laps.""")
+st.title("Welcome to Lap Splitter!")
+st.caption("A website where you can understand the pace you need to run to go fast, win races, and qualify for championships.")
 
-mins = st.number_input('Insert number of minutes', min_value=1)
-st.write('The number of minutes is ', mins)
+mins = st.number_input('Enter number of minutes', min_value=1)
 mins_secs = mins *60
+secs = st.number_input('Enter number of seconds', max_value=59)
+hundreths = st.number_input('Enter Seconds to Hundreths Place:', max_value=0.99)
+total_time_in_seconds = float(mins_secs + secs + hundreths)
+st.write("Your goal time in seconds is: ", total_time_in_seconds)
 
-secs = st.number_input('Insert number of seconds')
-st.write('The number of seconds is ', secs)
+#race_dist = st.number_input('Enter Race Distance:', min_value=800)
+#st.write('Race Distance ', race_dist)
+race_dist= st.radio(
+    "Select your race distance",
+    (800, 1500, 1600, 3000, 3200, 5000, 10000))
 
-hundreths = st.number_input('Enter Seconds to Hundreths Place:')
-st.write('Seconds to Hundreths: ', hundreths)
-hundreths_decimal = float(hundreths / 100)
-
-
-total_time_in_seconds = float(mins_secs + secs + hundreths_decimal)
-st.write(total_time_in_seconds)
-
-race_dist = st.number_input('Enter Race Distance:', min_value=800)
-st.write('Race Distance ', race_dist)
 num_hundreds = int(race_dist / 100)
-st.write('Number of Hundreds: ', num_hundreds)
 total_time_per_hundred = total_time_in_seconds / num_hundreds
-st.write('100 time: ', total_time_per_hundred)
-
 
 for i in range(1, num_hundreds+1):
     st.write('{0}00m split: '.format(i), round(i*total_time_per_hundred,2))
